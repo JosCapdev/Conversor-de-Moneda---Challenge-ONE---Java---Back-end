3cbd41070f3ee918fd13cdf83480a6ce4e564c7b
JosCapdev <josecapdevila512@gmail.com>
2025-11-12 18:21:35 -0300

    Agregamos la libreria Gson

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,12 @@
+import com.google.gson.Gson;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Principal {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        Gson gson = new Gson();
     }
 }
