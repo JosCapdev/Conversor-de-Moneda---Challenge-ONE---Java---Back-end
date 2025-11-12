1a74be2e2ce654b6bbb648fd0181470afe92510e
JosCapdev <josecapdevila512@gmail.com>
2025-11-12 18:34:50 -0300

    agregados en funcionalidades de ConversorMoneda

--- src/ConversorMoneda.java
+++ src/ConversorMoneda.java
@@ -1,2 +1,25 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConversorMoneda {
+    public Moneda buscaMoneda(String nombre){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/7a3e298b02a61a891173809d/latest/"+nombre);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Moneda.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error en la busqueda del tipo de Moneda.");
+        }
+    }
 }
