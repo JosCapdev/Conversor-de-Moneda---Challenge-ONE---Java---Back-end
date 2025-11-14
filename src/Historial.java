7c8dd15b734cb78c654499a03b6536278c493eb1
JosCapdev <josecapdevila512@gmail.com>
2025-11-14 14:00:15 -0300

    agregado de funcionalidades del historial

--- src/Historial.java
+++ src/Historial.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Historial {
+    private List<String> registros = new ArrayList<>();
+
+    public void agregarRegistro(String registro) {
+        registros.add(registro);
+    }
+
+    public void mostrarHistorial() {
+        if (registros.isEmpty()) {
+            System.out.println("⚠️ No hay conversiones registradas todavía.");
+        } else {
+            System.out.println("**********************************************************");
+            System.out.println("Historial de Conversiones:");
+            for (String r : registros) {
+                System.out.println("* " + r + "\n");
+            }
+        }
+    }
 }
